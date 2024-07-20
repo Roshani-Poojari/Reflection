8caf9e04a71d6ceb077a0112262afde3e8374704
Roshani-Poojari <roshanipoojari2002@gmail.com>
2024-07-20 19:46:21 +0530

    first commit

---
+++ Models/Account.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionDemo.Models
+{
+    internal class Account
+    {
+        private const double MIN_BALANCE = 500;
+
+        public int AccountNumber { get; set; }
+        public string Name { get; set; }
+        public double Balance { get; set; }
+
+        public Account(int accNumber, string name)
+        {
+            AccountNumber = accNumber;
+            Name = name;
+            Balance = MIN_BALANCE;
+        }
+
+        public Account(int accNumber, string name, double balance) : this(accNumber, name) 
+        {
+            if (balance < MIN_BALANCE)
+                Balance = MIN_BALANCE;
+            else
+                Balance = balance;
+        }
+
+        public bool Deposit(double amount)
+        {
+            Balance += amount;
+            return true;
+        }
+        public bool Withdraw(double amount)
+        {
+            if (Balance - amount < MIN_BALANCE)
+                return false;
+            Balance -= amount;
+            return true;
+        }
+
+        public override string ToString()
+        {
+            return $"Account no: {AccountNumber} \n" +
+                $"Account holder's name: {Name} \n" +
+                $"Balance: {Balance} \n" +
+                $"-----------------------------------------------------------";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using ReflectionDemo.Models;
+using System.Reflection;
+
+namespace ReflectionDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account1 = new Account(101, "Allen", 10000);
+            Account account2 = new Account(102, "Mary");
+
+            Assembly executing = Assembly.GetExecutingAssembly();
+
+            Type[] types = executing.GetTypes();
+            foreach (var item in types)
+            {
+                // Display each type 
+                Console.WriteLine($"Class : {item.Name}\n");
+                int methodCount = 0;
+                // Array to store methods 
+                MethodInfo[] methods = item.GetMethods();
+                foreach (var method in methods)
+                {
+                    // Display each method 
+                    Console.WriteLine($"--> Method : {method.Name}");
+                    methodCount++;
+                }
+                Console.WriteLine("\nNumber Of Methods: "+methodCount+"\n");
+                int propertyCount = 0;
+                // Array to store properties 
+                PropertyInfo[] properties = item.GetProperties();
+                foreach (var property in properties)
+                {
+                    // Display each property 
+                    Console.WriteLine($"--> Property : {property.Name}");
+                    propertyCount++;
+                }
+                Console.WriteLine("\nNumber Of Properties: " + propertyCount+"\n\n");
+
+
+                int constructorCount = 0;
+                // Array to store constructors
+                ConstructorInfo[] constructors = item.GetConstructors();
+                foreach (var constructor in constructors)
+                {
+                    // Display each constructor
+                    Console.WriteLine($"--> Constructor : {constructor.Name}");
+                    constructorCount++;
+                }
+                Console.WriteLine("\nNumber Of Constructors: " + constructorCount + "\n\n");
+            }
+        }
+    }
+}
+/*Reflection is the process of describing the metadata of types, methods and fields in a code. 
+ * The namespace System.Reflection enables you to obtain data about the loaded assemblies, the 
+ * elements within them like classes, methods and value types. */
